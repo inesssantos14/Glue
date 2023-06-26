28ff0b2ffac6294f3181e73254b75e81865e738e
Inês Serafim Santos <inesssantos2001@gmail.com>
2023-06-26 18:04:15 +0100

    [responsividade] menu

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,9 +1,21 @@
 const menu = document.querySelector('.menu');
+const mobileMenuButton = document.querySelector('button.mobile-button');
 
 function handleWindowScroll() {
-    if (window.scrollY >= 60) {
-        menu.classList.add('active')
+    if (window.innerWidth >= 600) {
+      if (window.scrollY >= 60) {
+        menu.classList.add('active');
+      } else {
+        menu.classList.remove('active');
+      }
     } else {
-        menu.classList.remove('active')
+      menu.classList.remove('active');
     }
+  }
+
+function handleMobileMenuClick() {
+    menu.classList.toggle('show');
 }
+
+window.addEventListener('scroll', handleWindowScroll);
+mobileMenuButton.addEventListener('click', handleMobileMenuClick);
